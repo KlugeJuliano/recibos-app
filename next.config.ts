9e5e888051f7ca20d3499b634fe3e84725299955
Juliano <kluge233@hotmail.com>
2025-05-19 00:12:19 -0300

    adicionado speedinsights

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Aplica para todas as rotas
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; img-src * data: blob:; object-src 'none';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

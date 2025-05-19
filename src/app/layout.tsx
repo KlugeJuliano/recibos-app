3f7d17c672f65782492be5fff2ed20caed8e3c53
Juliano <kluge233@hotmail.com>
2025-05-19 00:25:12 -0300

    desligado speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({
         <Providers session={session}>
           {children}
         </Providers>
-        <SpeedInsights />
+       {/* <SpeedInsights />  */}
       </body>
     </html>
   );
